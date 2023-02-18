15bc93b96431b6f5d26070261606abd57cbc9f5e
Nima Sayyah <nimasayyah@nimas-mbp.home>
2023-02-18 11:41:24 +0000

    files initialised

---
+++ codility_exercises.py
@@ -0,0 +1,23 @@
+# #OddOccurrencesInArray
+# # Find value that occurs in odd number of elements (55%)(how about if they are 3 same numbers?)
+# def unique_numbers(A):
+#     result = []
+#     for i in A:
+#         if A.count(i) == 1:
+#             result.append(i)
+#     return result
+# print(unique_numbers(A=[1,2,3,1,4,4,2,3,5,5,0]))
+
+#TapeEquilibrium
+#Minimize the value |(A[0] + ... + A[P-1]) - (A[P] + ... + A[N-1])| ()()
+def minimum_difference(A=[3,1,2,4,3]):
+    result = list()
+    P = [i for i in range(1,len(A))]
+    print(P)
+    for i in range(len(A)):
+        f = 
+        d = A[i]+
+
+# A=[3,1,2,4,3]
+# # P = range(len(A))
+# print(range(len(A)))
